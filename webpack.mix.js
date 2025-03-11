a63167f4ac66f49dd0b0c84e5ec072bb7bf166b9
norcenw <186290875+norcenw@users.noreply.github.com>
2025-03-11 23:00:18 +0100

    11.03.2025

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,5 +5,6 @@ mix
   .js("src/js/dict.js", "js/dict.js")
   .sass("src/scss/style.scss", "css/style.css")
   .copy("src/**.html", "docs")
+  .copyDirectory("src/assets", "docs/assets")
   .setPublicPath("docs")
   .version();
