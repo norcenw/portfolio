f205952ae3e1028f8c1db5ba90f8dc108e9ee964
norcenw <186290875+norcenw@users.noreply.github.com>
2025-03-11 22:16:47 +0100

    11.03.2025

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require("laravel-mix");
+
+mix
+  .js("src/js/app.js", "js/app.js")
+  .js("src/js/dict.js", "js/dict.js")
+  .sass("src/scss/style.scss", "css/style.css")
+  .copy("src/**.html", "docs")
+  .setPublicPath("docs")
+  .version();
